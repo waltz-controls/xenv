1b05ed221bece8994c995c3b3da0c804c4bf727e
ingvord <mail@ingvord.ru>
2021-06-01 14:54:48 +0200

    Progress

---
+++ tango/admin/register.sh
@@ -0,0 +1,7 @@
+/usr/local/bin/tango_admin --add-server HeadQuarter/dev HeadQuarter dev/xenv/hq
+/usr/local/bin/tango_admin --add-server HeadQuarter/dev XenvManager dev/xenv/manager
+/usr/local/bin/tango_admin --add-server HeadQuarter/dev ConfigurationManager dev/xenv/config
+/usr/local/bin/tango_admin --add-server StatusServer2/dev StatusServer2 dev/xenv/status_server
+/usr/local/bin/tango_admin --add-server DataFormatServer/dev DataFormatServer dev/xenv/dfs
+/usr/local/bin/tango_admin --add-server CamelIntegration/dev CamelIntegration dev/xenv/camel
+/usr/local/bin/tango_admin --add-server PreExperimentDataCollector/dev PreExperimentDataCollector dev/xenv/predator
